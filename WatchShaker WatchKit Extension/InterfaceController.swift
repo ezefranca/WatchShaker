6bfb0ad8d63913fce0c626c9ae28da99cac5c3d0
ezefranca <ezequiel.ifsp@gmail.com>
2016-12-09 23:41:48 -0200

    Initial Project

---
+++ WatchShaker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,47 @@
+//
+//  InterfaceController.swift
+//  WatchShaker WatchKit Extension
+//
+//  Created by Ezequiel on 09/12/16.
+//  Copyright © 2016 Ezequiel França @ezefranca. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        // Configure interface objects here.
+    }
+    
+    var shaker:WatchShaker = WatchShaker(shakeSensibility: .shakeSensibilityNormal, delay: 0.2)
+    
+    override func willActivate() {
+        
+        super.willActivate()
+        shaker.delegate = self
+        shaker.start()
+    }
+    
+    override func didDeactivate() {
+        
+        super.didDeactivate()
+        shaker.stop()
+        
+    }
+    
+}
+
+extension InterfaceController: WatchShakerDelegate
+{
+    func didShake() {
+        print("YOU SHAKE YOUR ⌚️⌚️⌚️")
+    }
+    
+    func error(with error: Error) {
+        print(error.localizedDescription)
+    }
+}
